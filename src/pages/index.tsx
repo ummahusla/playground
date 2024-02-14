ed883f897e3c021ac6cf2a1a61b83c8ec102a06f
Edvins Antonovs <edvins.antonovs@giraffe360.com>
2024-02-14 16:22:32 +0200

    feat: broken af

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-import Stories from '@/components/Stories';
+// import Stories from '@/components/Stories';
+import PanoramicTour from '@/components/PanoramicTour';
 
 function Home() {
   return (
     <div className="flex justify-center">
-      <Stories />
+      {/* <Stories /> */}
+
+      <div
+        className="w-[390px] h-[844px]"
+        style={{
+          backgroundColor: '#f5f5f5',
+        }}
+      >
+        <PanoramicTour imagePath="/assets/pano_1.jpg" />
+      </div>
     </div>
   );
 }
